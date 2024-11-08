09cffe09cb6dbbde61a708b8ffa05c10f0fde9fb
karolina.drozdz <kdrozdz0609@gmail.com>
2024-11-08 16:19:44 +0100

    official

--- tasks.py
+++ tasks.py
@@ -8,24 +8,21 @@ from sklearn import datasets
 # Then copy and paste code asked for below in between the dashed lines.
 # Do not import additional packages.
  
-# Task 1:
-# Instructions:
-# In the notebook, you wrote a function that implements an MLP with 2 hidden layers.
-# The function should accept a vector of weights and a matrix X that stores input feature
-# vectors in its **columns**.
-# The name of the function should be my_mlp.
- 
 # Copy and paste the code for that function here:
 # -----------------------------------------------
-def my_mlp(w, X, sigma=np.tanh):
-    W1 = (w[0:4*6]).reshape(4,6)
-    W2 = (w[4*6:7*4+6*4]).reshape(7,4)
-    W3 = (w[7*4+6*4:]).reshape(1,7)
-   
-    a1 = sigma(W1 @ X)
-    a2 = sigma(W2 @ a1)
-    f  = sigma(W3 @ a2)
-    return f [0, 0, :, :]
+def my_mlp(w, X, sigma = np.tanh):
+    # Construct between layer connection weights matrices W1, W2, and W3
+    W1 = w[:4 * 6].reshape(4, 6)
+    W2 = w[4 * 6:4 * 6 + 7 * 4].reshape(7, 4)
+    W3 = w[4 * 6 + 7 * 4:].reshape(1, 7)
+    
+    # Implement the equations
+    a1 = sigma(W1 @ X),  #   input -> layer 1
+    a2 = sigma(W2 @ a1), # layer 1 -> layer 2
+    f  = sigma(W3 @ a2) # layer 2 -> output
+    
+    return f[0, 0, :, :]
+
 # -----------------------------------------------
  
 # Task 2:
@@ -33,14 +30,15 @@ def my_mlp(w, X, sigma=np.tanh):
 # In the notebook, you wrote a function that implements a loss function for training
 # the MLP implemented by my_mlp of Task 1.
 # The function should accept a vector of weights, a matrix X that stores input feature
-# vectors in its **columns**, and a vector y that stores the target labels (-1 or +1).
+# vectors in its *columns*, and a vector y that stores the target labels (-1 or +1).
+
 # The name of the function should be MSE_func.
  
 # Copy and paste the code for that function here:
 # -----------------------------------------------
 def MSE_func(w, X, y): 
     f = my_mlp(w, X)
-    MSE = np.sum((y-f)**2)
+    MSE = np.sum((f-y)**2)
     return MSE
 # -----------------------------------------------
  
@@ -54,11 +52,12 @@ def MSE_func(w, X, y):
 # as its components.
 # The name of the function should be dR.
  
+
 # Copy and paste the code for that function here:
 # -----------------------------------------------
 def dR(beta, x, y):
-    dbeta_0 = 2*np.mean((beta[0] + beta[1]*x - y))   # implement the above formula for dR/dβ₀
-    dbeta_1 = 2*np.mean((beta[0] + beta[1]*x - y)*x) # implement the above formula for dR/dβ₁
+    dbeta_0 = 2*np.mean(beta[0]+beta[1]*x - y) # implement the above formula for dR/dβ₀
+    dbeta_1 = 2*np.mean((beta[0]+beta[1]*x - y)*x) # implement the above formula for dR/dβ₁
     return np.array([dbeta_0, dbeta_1])
  
 # -----------------------------------------------
